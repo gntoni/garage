1fd9964161a6193324cd6e781d457d48cc5b7ef7
Jonathon Shen <jshen100@gmail.com>
2018-07-12 13:22:46 -0700

    Add unit testing

--- tests/algos/test_trpo.py
+++ tests/algos/test_trpo.py
@@ -1,3 +1,5 @@
+import unittest
+
 import gym
 import numpy as np
 import theano.tensor as TT
@@ -32,35 +34,40 @@ def naive_relu(x):
     return TT.max(x, 0)
 
 
-def test_trpo_relu_nan():
-    env = TheanoEnv(DummyEnv())
-    policy = GaussianMLPPolicy(
-        env_spec=env.spec, hidden_nonlinearity=naive_relu, hidden_sizes=(1, ))
-    baseline = ZeroBaseline(env_spec=env.spec)
-    algo = TRPO(
-        env=env,
-        policy=policy,
-        baseline=baseline,
-        n_itr=1,
-        batch_size=1000,
-        max_path_length=100,
-        step_size=0.001)
-    algo.train()
-    assert not np.isnan(np.sum(policy.get_param_values()))
+class TestTRPO(unittest.TestCase):
+    def test_trpo_relu_nan(self):
+        env = TheanoEnv(DummyEnv())
+        policy = GaussianMLPPolicy(
+            env_spec=env.spec,
+            hidden_nonlinearity=naive_relu,
+            hidden_sizes=(1, ))
+        baseline = ZeroBaseline(env_spec=env.spec)
+        algo = TRPO(
+            env=env,
+            policy=policy,
+            baseline=baseline,
+            n_itr=1,
+            batch_size=1000,
+            max_path_length=100,
+            step_size=0.001)
+        algo.train()
+        assert not np.isnan(np.sum(policy.get_param_values()))
 
+    # https://github.com/rlworkgroup/garage/issues/186
+    test_trpo_relu_nan.broken = True
 
-def test_trpo_deterministic_nan():
-    env = TheanoEnv(DummyEnv())
-    policy = GaussianMLPPolicy(env_spec=env.spec, hidden_sizes=(1, ))
-    policy._l_log_std.param.set_value([np.float32(np.log(1e-8))])
-    baseline = ZeroBaseline(env_spec=env.spec)
-    algo = TRPO(
-        env=env,
-        policy=policy,
-        baseline=baseline,
-        n_itr=10,
-        batch_size=1000,
-        max_path_length=100,
-        step_size=0.01)
-    algo.train()
-    assert not np.isnan(np.sum(policy.get_param_values()))
+    def test_trpo_deterministic_nan(self):
+        env = TheanoEnv(DummyEnv())
+        policy = GaussianMLPPolicy(env_spec=env.spec, hidden_sizes=(1, ))
+        policy._l_log_std.param.set_value([np.float32(np.log(1e-8))])
+        baseline = ZeroBaseline(env_spec=env.spec)
+        algo = TRPO(
+            env=env,
+            policy=policy,
+            baseline=baseline,
+            n_itr=10,
+            batch_size=1000,
+            max_path_length=100,
+            step_size=0.01)
+        algo.train()
+        assert not np.isnan(np.sum(policy.get_param_values()))
